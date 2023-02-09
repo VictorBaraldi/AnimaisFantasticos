feaae20af4ae5c0178747433a85dc8a27b5e6f72
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2023-02-09 11:44:51 -0300

    refatorando anima Numeros para classe

--- script/modules/animaNumeros.js
+++ script/modules/animaNumeros.js
@@ -1,32 +1,49 @@
-export default function animaNumeros() {
-  const numeros = document.querySelectorAll(".numeros-animal span");
+export default class AnimaNumeros {
+  constructor(numeros, observerClass, observerTarget) {
+    this.numeros = document.querySelectorAll(numeros);
+    this.observerClass = observerClass;
+    this.observerTarget = document.querySelector(observerTarget);
+    this.handleMutation = this.handleMutation.bind(this);
+  }
 
-  function numbersInit() {
+  static incrementarNumero(value) {
+    const total = +value.innerText;
+    const incremento = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start += incremento;
+      value.innerText = start;
+      if (start > total) {
+        value.innerText = total;
+        clearInterval(timer);
+      }
+    }, 25 * Math.random());
+  }
+
+  numbersInit() {
     // basicamente pega o valor de dentro do number e substitui pelo proporcional do valor a cada 25ms
-    numeros.forEach((value) => {
-      const total = +value.innerText;
-      const incremento = Math.floor(total / 100);
-      let start = 0;
-      const timer = setInterval(() => {
-        start += incremento;
-        value.innerText = start;
-        if (start > total) {
-          value.innerText = total;
-          clearInterval(timer);
-        }
-      }, 25 * Math.random());
+    this.numeros.forEach((value) => {
+      this.constructor.incrementarNumero(value);
     });
   }
-  let observer;
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains("ativo")) {
+
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
       // verifica se o observado possui a classe ativo, que so tem quando está visivel na tela
-      observer.disconnect(); // a partir do momento que observa a classe ativo, o observador é disconectado e para de observar
-      numbersInit(); // função de animação dos numeros inicia
+      this.observer.disconnect(); // a partir do momento que observa a classe ativo, o observador é disconectado e para de observar
+      this.numbersInit(); // função de animação dos numeros inicia
     }
   }
-  const observerTarget = document.querySelector(".numeros"); // selecionando o elemento
-  observer = new MutationObserver(handleMutation); // recebe o callback, que vai ser executado quando mudar
 
-  observer.observe(observerTarget, { attributes: true }); // definimos qual o target o observer deve olhar
+  addMutationObserver() {
+    this.observer = new MutationObserver(this.handleMutation); // recebe o callback, que vai ser executado quando mudar
+    this.observer.observe(this.observerTarget, { attributes: true }); // definimos qual o target o observer deve olhar
+  }
+
+  init() {
+    if (this.numeros.length && this.observerTarget) {
+      this.addMutationObserver();
+    }
+    return this;
+  }
 }
